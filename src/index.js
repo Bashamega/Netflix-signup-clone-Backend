2c306412a0e9512b660b722d0a614ecf959105ec
Bashamega <adambashaahmednaji@gmail.com>
2024-02-10 09:20:36 +0200

    Updated api

--- src/index.js
+++ src/index.js
@@ -4,9 +4,36 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.get('/netflixpayment', (req, res)=>{
-    res.status(500).json(
-        {message: "Subscribe and like"}
-    )
+    try {
+        const {cardNumber, expirationDate, cvv, firstName, lastName} = req.query
+        if(!cardNumber){
+            res.status(500).json({message:'Please provide a Card Number'})
+
+        }else if(!expirationDate){
+            res.status(500).json({message:'Please provide an expiration date'})
+
+        }else if(!cvv){
+            res.status(500).json({message:'Please provide a CVV '})
+
+        }else if(!firstName){
+            res.status(500).json({message:'Please provide a fist name'})
+
+        }else{
+            const obj = {
+                cardNumber:cardNumber,
+                expirationDate:expirationDate,
+                cvv:cvv,
+                firstName:firstName,
+                lastName:lastName
+            }
+            res.status(500).json(obj)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'A technical error has occurred'
+        })
+    }
 })
 
 app.listen(3001, ()=>{
